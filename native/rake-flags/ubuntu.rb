fd752369baa067eb0d9e637995313fe10ecc183c
narkisr <ronen@Uranus.(none)>
2010-06-24 23:23:56 +0300

    last fixup

--- native/rake-flags/ubuntu.rb
+++ native/rake-flags/ubuntu.rb
@@ -6,5 +6,5 @@ JAVA_HOME = '/usr/lib/jvm/java-6-sun'
 
 INCLUDES = "-I#{JAVA_HOME}/include -I#{JAVA_HOME}/include/linux -I#{FUSE_HOME}/include/fuse"
 CFLAGS = "-Wall -g -fPIC #{DEPFLAGS}"
-server_path = "amd64" unless `uname -a`.include?("i686") else "i386" 
+server_path = (`uname -a`.include?("i686") && "i386") || "amd64"
 LDPATH = "-L#{FUSE_HOME}/lib -L#{JAVA_HOME}/jre/lib/#{server_path}/server"
